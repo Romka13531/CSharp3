8b0d7a70bf17bfa1a1948c9456e3e919a301d3cf
Romka13531 <45291876+Romka13531@users.noreply.github.com>
2018-12-27 14:18:55 +0300

    Add files via upload

---
+++ Programm.cs
@@ -0,0 +1,15 @@
+using System;
+using MyCollections;
+
+    public class Programm
+    {
+        public static void Main()
+        {
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(1);
+        stack.Push(4);
+        stack.Push(3);
+        Console.WriteLine(stack.Length);
+            Console.ReadKey();
+        }
+    }
